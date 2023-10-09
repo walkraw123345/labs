53df7651e0f83cc9193e06822d208b2946ee43c0
walkraw123345 <93652720+walkraw123345@users.noreply.github.com>
2023-10-09 16:21:59 +0300

    Add files via upload

---
+++ ConsoleApplication2/main.cpp
@@ -0,0 +1,37 @@
+//Файл File_Main.cpp
+#include <iostream>        // Подключаем библиотеки       
+
+using namespace std;
+int task_5_1(); // Объявление функции 
+int task_5_2(); 
+int task_5_3();
+int task_5_4();
+int task_5_5();
+
+int task_9_1();
+int task_9_2();
+int task_9_3();
+int task_9_4();
+int task_9_5();
+
+int main()
+{
+		// lab_5.cpp
+	//task_5_1();
+	//task_5_2();
+	//task_5_3();
+	//task_5_4();
+	//task_5_5();
+
+		//lab_9.cpp
+	//task_9_1();
+	//task_9_2();
+	//task_9_3();
+	//task_9_4();
+	//task_9_5();
+
+
+
+
+
+}
